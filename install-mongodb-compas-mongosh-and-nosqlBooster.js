c12755bb206bca901b588ea65c1e842343010760
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-03 23:05:51 +0600

    add command

---
+++ install-mongodb-compas-mongosh-and-nosqlBooster.js
@@ -0,0 +1,42 @@
+// install mongodb compass mongosh and nosqlBooster
+
+// mongodb graphical user interface
+
+
+1. chrome -> mongodb download -> community server download
+2. select windos64
+3. start install -> next ->  complete -> next -> next -> install
+4. open it
+5. connect
+6. open mongosh editor   -> show dbs -> show database
+7. use practice  -> create database
+8. db.createCollection("test")  -> create collection
+9. db.getCollection("test").insertOne({name: "trying to learn mongodb"})
+10. db.getCollection("test").find()    -> get data
+
+
+
+
+// if want to run command of mongosh in cmd
+1. google -> mongodb shell download  -> select msi package -> download 
+
+//find the path
+2. go to c drive -> program file -> mongodb -> server -> 10.0 -> bin -> copy this path
+3. add this path in environment variable
+4. open cmd
+5. show dbs
+6. use practice
+7. db.getCollection("test").find()
+
+
+
+
+// use another graphical user interface
+
+// chosse studio 3t or nosqlBooster
+
+// nosqlBooster
+1. google -> nosqlBooster -> download -> select windos - > download
+2. open and install 
+3. open it after install  -> connect -> new collection -> test collection -> ok -> name =localhost -> save and connect
+4. ctrl + enter -> for run the command
